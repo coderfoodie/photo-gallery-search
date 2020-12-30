75f06e9a6b8b5d7ce9be0ef37f12ae037be591c0
coderfoodie <lsomodio@gmail.com>
2020-12-30 16:52:25 +0800

    Update index.php
    added some codes

--- index.php
+++ index.php
@@ -3,18 +3,20 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Photo Gallery</title>
+    <title>Photo Gallery in PHP</title>
     <link rel="stylesheet" href="./css/styles.css">
 </head>
 <body>
-    
-    <?php
-        $files = glob("./img/*");
+    <div class="gallery">
+        <?php
+            $files = glob("./img/*");
 
-        foreach ($files as $filename) {
-            echo "<div><img src='".$filename."'></div><br>";
-        }
-    ?>
+            foreach ($files as $filename) {
+                echo `<a href='gallery.php?img=".$filename."'>
+                <img class="pics" src='".$filename."' alt="" /></a>`;
+            }
+        ?>
+    </div>
 
 </body>
 </html>
